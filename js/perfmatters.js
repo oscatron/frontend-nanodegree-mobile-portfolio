42b44a00dca57284ca0f0aafdb6ca638c410e2ea
oscatron <oscatron@gmail.com>
2018-01-02 21:29:34 -0500

    Update perfmatters.js

--- js/perfmatters.js
+++ js/perfmatters.js
@@ -1,5 +1,11 @@
 // Measuring the Critical Rendering Path with Navigation Timing
 // https://developers.google.com/web/fundamentals/performance/critical-rendering-path/measure-crp
+ (function(w,g){w['GoogleAnalyticsObject']=g;
+ w[g]=w[g]||function(){(w[g].q=w[g].q||[]).push(arguments)};w[g].l=1*new Date();})(window,'ga');
+
+      // Optional TODO: replace with your Google Analytics profile ID.
+      ga('create', 'UA-XXXX-Y');
+      ga('send', 'pageview');
 
 function logCRP() {
   var t = window.performance.timing,
